7d7896afd626bb624000a90aea637866daaa0da4
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2021-07-12 21:57:05 +0200

    Checking other data structures [ test ] + move products to another file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+import { products, productsObj } from "./assets/products";
 
-import SearchInput from './Components/SearchInput';
+import SearchInput from "./Components/SearchInput";
 
 const useStyles = makeStyles({
   App: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100vh',
-    alignItems: 'center'
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    alignItems: "center",
+    backgroundColor: "#fad390",
   },
 });
 
 const App = () => {
   const classes = useStyles();
 
-  const allProducts = [
-    'apple', 'apricot', 'pineapple', 'tomato', 'carrot', 'cabbage', 'coconut', 'cucumber', 'celery', 'couliflower', 'courgette', 'lettuce', 'raddish'
-  ];
-
   return (
     <div className={classes.App}>
       <h3>FoxList</h3>
-      <SearchInput allProducts={allProducts} />
+      <SearchInput allProducts={products} productsObj={productsObj} />
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/Components/SearchInput.js
+++ src/Components/SearchInput.js
@@ -1,34 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
-const SearchInput = ({ allProducts }) => {
-	const [products, setProducts] = useState([]);
+const SearchInput = ({ allProducts, productsObj }) => {
+  const [products, setProducts] = useState([]);
 
-	const filterVegArray = (e) => {
-		const searchValue = e.target.value;
-		const filterdArray = allProducts.filter(p => p.toLowerCase().includes(searchValue.toLowerCase()) && p[0] === searchValue[0]?.toLowerCase());
+  console.log(productsObj);
 
-		setProducts(searchValue.length > 0 ? filterdArray : []);
-	};
+  const filterVegArray = e => {
+    const searchValue = e.target.value;
+    const filterdArray = allProducts.filter(
+      p =>
+        p.toLowerCase().includes(searchValue.toLowerCase()) &&
+        p[0] === searchValue[0]?.toLowerCase(),
+    );
 
-	return (
-		<Autocomplete
-			style={{ width: 500 }}
-			freeSolo
-			autoComplete
-			autoHighlight
-			options={products}
-			renderInput={(params) => (
-				<TextField {...params}
-					onChange={e => filterVegArray(e)}
-					variant="outlined"
-					label="Fox tail"
-				/>
-			)}
-		/>
-	)
-}
+    setProducts(searchValue.length > 0 ? filterdArray : []);
+  };
+
+  return (
+    <Autocomplete
+      style={{ width: 500 }}
+      freeSolo
+      autoComplete
+      autoHighlight
+      options={products}
+      renderInput={params => (
+        <TextField
+          {...params}
+          onChange={e => filterVegArray(e)}
+          variant="outlined"
+          label="Fox tail"
+        />
+      )}
+    />
+  );
+};
 
 export default SearchInput;
