32b5f731edb81105b8ce5f2a5513414e9da19adb
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2021-12-09 13:41:08 +0100

    Add welcome text with button

--- src/Pages/FoxListStartupPage.tsx
+++ src/Pages/FoxListStartupPage.tsx
@@ -1,19 +1,48 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "../Components/TextField";
 import { IProduct, IProductsObject, IVegsArrays } from '../interfaces/products/products.interfaces';
 import DynamicProductsList from "../Components/DynamicProductsList";
+import ButtonRound from "../Components/ButtonRound";
+import { ReactComponent as PlusIcon } from "../assets/icons/plus-round.svg";
 
 interface IFoxListProps {
   productsObject: IProductsObject;
 }
 
+const useStyles = makeStyles({
+  WelcomeHeader: {
+    color: "#1c5f7e",
+    padding: "1rem 0 "
+  },
+
+  WelcomeText: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "inherit",
+  },
+
+  Icon: {
+    fill: "#1c5f7e",
+    width: "70px",
+    height: "70px",
+    cursor: "pointer",
+  },
+});
+
 const FoxListStartupPage = (foxListProps: IFoxListProps): JSX.Element => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [isFocused, setIsFocused] = useState<boolean>(false)
+  const [welcomePage, setWelcomePage] = useState<boolean>(true);
   const { productsObject } = foxListProps;
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const filteredProducts = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
+  const classes = useStyles();
+  const { Icon, WelcomeText, WelcomeHeader } = classes;
+
+
+  const filterProducts = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
     let language: string = "english";
     const searchValue: string = e.target.value;
     const firstInputLetter: string = searchValue.charAt(0).toUpperCase();
@@ -32,22 +61,42 @@ const FoxListStartupPage = (foxListProps: IFoxListProps): JSX.Element => {
     console.log("go");
   };
 
-  const handleClick = () => {
+  const handleInputFocus = () => {
     inputRef.current?.focus();
     setIsFocused(true);
   }
 
+  const createList = () => {
+    setWelcomePage(false);
+  }
+
   return (
-    <form className="form" onSubmit={(e) => handleSubmit(e)}>
-      <TextField
-        onChange={e => filteredProducts(e)}
-        reference={inputRef}
-      />
-      <DynamicProductsList
-        products={products}
-        handleFocus={handleClick}
-      />
-    </form>
+    <Fragment>
+      {
+        welcomePage ? (
+          <div className={WelcomeText}>
+            <h1 className={WelcomeHeader}>Welcome to FoxList 😉</h1>
+            <ButtonRound
+              handleClick={createList}
+              sizeBig={true}>
+              <PlusIcon className={Icon} />
+            </ButtonRound>
+            <h2 className={WelcomeHeader}>Hit the button to start.</h2>
+          </div>
+        ) : (
+          <form className="form" onSubmit={(e) => handleSubmit(e)}>
+            <TextField
+              onChange={e => filterProducts(e)}
+              reference={inputRef}
+            />
+            <DynamicProductsList
+              products={products}
+              handleFocus={handleInputFocus}
+            />
+          </form>
+        )
+      }
+    </Fragment>
   );
 };
 
