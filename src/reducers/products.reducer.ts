d933c5023f78a24fafba4d6acc495a43058a8c85
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2021-12-15 18:59:47 +0100

    Create productsReducer

---
+++ src/reducers/products.reducer.ts
@@ -0,0 +1,49 @@
+import { Dispatch, Reducer } from "react";
+import { IProduct } from "../interfaces/products/products.interfaces";
+
+export type Action = 
+  | {type: "ADD_PRODUCT", product: IProduct}
+  | {type: "REMOVE_PRODUCT", product: IProduct};
+
+export interface State  {
+  productsList: IProduct[],
+  dispatch?: Dispatch<Action>
+}
+
+export const productsReducer: Reducer<State, Action> = (state: State, action: Action ): State => {
+  const existingProduct = state.productsList.find(p => p.id === action.product.id);
+  let updatedProductsList: IProduct[];
+  
+  switch (action.type) {
+    case "ADD_PRODUCT": 
+      if (existingProduct) {
+        updatedProductsList = state.productsList.map(p => (
+          p.id === action.product.id
+            ? { ...p, quantity: p.quantity += 1 }
+            : p
+        ));
+        return { productsList: updatedProductsList };
+      } else {
+        const newProduct = {
+          ...action.product,
+          quantity: 1,
+        }
+        return { productsList: [...state.productsList, newProduct] };
+    }
+
+    case "REMOVE_PRODUCT":
+      if (existingProduct && existingProduct.quantity > 1) {
+        updatedProductsList = state.productsList.map(p => (
+          p.id === action.product.id
+            ? { ...p, quantity: p.quantity === 0 ? p.quantity = 0 : p.quantity -= 1 }
+            : p
+        ))
+        return { productsList: updatedProductsList }
+      } else {
+        updatedProductsList = state.productsList.filter(p => p.id !== action.product.id);
+        return { productsList: updatedProductsList }
+      }
+    default:
+      return { productsList: state.productsList };
+  }
+}
