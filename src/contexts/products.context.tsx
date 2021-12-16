ecb411bcbdfb591d32bf7232a9a5d15cb3ab50d8
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2021-12-16 21:45:51 +0100

    Change type to interface

--- src/contexts/products.context.tsx
+++ src/contexts/products.context.tsx
@@ -1,11 +1,11 @@
 import { createContext, FC, useReducer } from 'react'
-import { productsReducer, State } from '../reducers/products.reducer';
+import { productsReducer, IState } from '../reducers/products.reducer';
 
-const defaultState: State = {
+const defaultState: IState = {
   productsList: [],
 }
 
-export const ProductsContext = createContext<State>(defaultState);
+export const ProductsContext = createContext<IState>(defaultState);
 
 export const ProductsProvider: FC = ({ children }) => {
   const [{ productsList }, dispatch] = useReducer(productsReducer, defaultState)
