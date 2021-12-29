e61e89051c2db71bae6bb2bc013fd2d01f6f6622
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2021-12-29 11:41:13 +0100

    Change button styles when item added + Add remove and add information when item added to productsList

--- src/Components/ProductsListItem.tsx
+++ src/Components/ProductsListItem.tsx
@@ -1,6 +1,7 @@
 // import { Dispatch, SetStateAction, useEffect, useState, useCallback, useMemo } from "react";
 import { useContext, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import useToggle from '../hooks/useToggle';
 import ButtonRound from "./ButtonRound";
 import { ReactComponent as PlusIcon } from "../assets/icons/plus-round.svg";
 import { ReactComponent as MinusIcon } from "../assets/icons/minus-round.svg";
@@ -15,6 +16,7 @@ const useStyles = makeStyles({
     justifyContent: "space-between",
     alignItems: "center",
     height: "40px",
+    position: "relative",
 
     "&:hover": {
       cursor: "default",
@@ -33,7 +35,7 @@ const useStyles = makeStyles({
     width: "25px",
     height: "25px",
     alignItems: "flex-end",
-    background: "#f1c40f",
+    background: "#1c5f7e",
     boxShadow: "0px 0px 4px 1px #227093",
     transition: "all .2s",
     margin: "0 .2rem 0 .2rem",
@@ -46,37 +48,34 @@ const useStyles = makeStyles({
     "&:active": {
       boxShadow: "0px 0px 2px 1px #227093",
     },
+
   },
+
   Button_disabled: {
-    width: "25px",
-    height: "25px",
-    borderRadius: "50%",
-    margin: "0 .2rem 0 .2rem",
-    opacity: ".7",
-    boxShadow: "0px 0px 4px 1px #227093",
-    marginRight: "5px",
+    opacity: ".5",
+    boxShadow: "0px 0px 0px 0px #227093",
+    backgroundColor: "#1c5f7e",
 
     "&:hover": {
-      boxShadow: "0px 0px 4px 1px #227093",
+      boxShadow: "0px 0px 0px 0px #227093",
     },
 
     "&:active": {
-      boxShadow: "0px 0px 4px 1px #227093",
+      boxShadow: "0px 0px 0px 0px #227093",
     },
   },
-  Icon: {
+  IconNoItems: {
+    fill: "#dff9fb",
+    width: "25px",
+    height: "25px",
+    cursor: "pointer"
+  },
+  IconWithItems: {
     fill: "#1c5f7e",
     width: "25px",
     height: "25px",
     cursor: "pointer"
   },
-
-  Icon_disabled: {
-    fill: "#1c5f7e",
-    width: "25px",
-    height: "25px",
-  },
-
   Count: {
     marginRight: ".5rem",
   },
@@ -84,6 +83,25 @@ const useStyles = makeStyles({
     display: "flex",
     alignItems: "center",
   },
+  Snackbar: {
+    position: "absolute",
+    transition: ".3s",
+    right: "-95px",
+  },
+  Add: {
+    right: "-80px",
+  },
+  SnackbarHidden: {
+    visibility: "hidden",
+    opacity: 0
+  },
+  SnackbarVisible: {
+    visibility: "visible",
+    right: "-85px",
+  },
+  SnackbarVisibleAdd: {
+    right: "-75px"
+  }
 });
 
 interface IProductsListItemProps {
@@ -94,12 +112,14 @@ const ProductsListItem = ( // useMemo or useCallback
   props: IProductsListItemProps
 ) => {
   const { productsList, dispatch } = useContext(ProductsContext);
-  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true)
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+  const [isItemAdded, setIsItemAdded] = useState<boolean>(false);
+  const [isItemRemoved, setIsItemRemoved] = useState<boolean>(false);
   const { product } = props;
   const { name } = product;
 
   const classes = useStyles();
-  const { ListItem, Item, CountWrap, Count, ButtonStyles, Button_disabled, Icon, Icon_disabled } = classes;
+  const { ListItem, Item, CountWrap, Count, ButtonStyles, Button_disabled, IconNoItems, IconWithItems, Snackbar, Add, SnackbarHidden, SnackbarVisible, SnackbarVisibleAdd } = classes;
 
   // using data from productsList to display quantity in dynamic list
   const currentProduct: IProduct | undefined = productsList?.find(p => p.name === product.name)
@@ -108,24 +128,46 @@ const ProductsListItem = ( // useMemo or useCallback
     !currentProduct?.quantity ? setIsButtonDisabled(true) : setIsButtonDisabled(false);
   }, [currentProduct?.quantity])
 
+  const handleAddProduct = (): void => {
+    dispatch?.({ type: "ADD_PRODUCT", product });
+    if (!currentProduct?.quantity) {
+      setIsItemAdded(true);
+      setTimeout(() => setIsItemAdded(false), 500);
+    }
+  }
+
+  const handleRemoveProduct = (): void => {
+    dispatch?.({ type: "REMOVE_PRODUCT", product });
+    if (currentProduct?.quantity === 1) {
+      setIsItemRemoved(true);
+      setTimeout(() => setIsItemRemoved(false), 500);
+    }
+  }
+
   return (
     <li className={ListItem}>
       <div className={Item}>
         <ButtonRound
-          handleClick={() => dispatch?.({ type: "ADD_PRODUCT", product })}
-          className={ButtonStyles}>
-          <PlusIcon className={Icon} />
+          handleClick={handleAddProduct}
+          className={!!currentProduct?.quantity ? "" : ButtonStyles}
+        >
+          <PlusIcon className={!!currentProduct?.quantity ? IconWithItems : IconNoItems} />
         </ButtonRound>
         {name}
       </div>
       <div className={CountWrap}>
         <span className={Count}>{currentProduct?.quantity ?? 0}</span>
         <ButtonRound
-          handleClick={() => dispatch?.({ type: "REMOVE_PRODUCT", product })}
-          className={isButtonDisabled ? Button_disabled : ButtonStyles}>
-          <MinusIcon className={isButtonDisabled ? Icon_disabled : Icon} />
+          handleClick={handleRemoveProduct}
+          className={
+            isButtonDisabled
+              ? Button_disabled
+              : ""}>
+          <MinusIcon className={!!currentProduct?.quantity ? IconWithItems : IconNoItems} />
         </ButtonRound>
       </div>
+      <div className={`${Snackbar} ${Add} ${isItemAdded ? SnackbarVisibleAdd : SnackbarHidden}`}>Added 🤟</div>
+      <div className={`${Snackbar} ${isItemRemoved ? SnackbarVisible : SnackbarHidden} `}>Removed 👌</div>
     </li >
   );
 };
